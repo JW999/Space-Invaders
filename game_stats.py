332cd0074fe2565f4b49268a7adc87cbd11a17d7
HMalkawi <husam.malkawi@hotmail.com>
2017-01-26 07:49:29 +0200

    Added the building blocks for a scoring system.

--- game_stats.py
+++ game_stats.py
@@ -7,9 +7,9 @@ class GameStats():
         self.reset_stats()
 
         # Initialize game state variables.
-        self.first_time = True
         self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
